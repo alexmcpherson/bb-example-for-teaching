5823cd7736f8cd5fa18bcaf4563bd499f23caa4a
Alex McPherson <alex.m.mcpherson@gmail.com>
2012-03-10 16:16:51 -0700

    Main routes

--- web.rb
+++ web.rb
@@ -1,5 +1,21 @@
 require 'sinatra'
 
+
+
+
+
+
+get '/:key' do
+	# Lookup URL by key, and redirect in that direction
+	params[:key]
+end
+
+post '/url' do
+	# save url and pair it with next shortest available key
+end
+
 get '/' do
-	"Hello world"
+	"URL shortener. Dot com."
+	# Show all url's
+	# show bookmarklet to shorten URL
 end
